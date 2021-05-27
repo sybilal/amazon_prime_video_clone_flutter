f4b10c35684ce1b783750fd05e9e38ab0c41dcd6
sybilal <sbilalahmed95@gmail.com>
2021-05-27 16:42:50 +0400

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -44,32 +44,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello Amazon Prime ;)',
+        body: CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          floating: true,
+          backgroundColor: kBackgroundColor,
+          title: Center(
+            child: Container(
+              child: Column(
+                children: [
+                  Image(
+                      width: 80,
+                      image: AssetImage('assets/images/prime_logo.png'))
+                ],
+              ),
             ),
-          ],
+          ),
         ),
-      ),
-    );
+        SliverList(
+          delegate:
+              SliverChildBuilderDelegate((BuildContext context, int index) {
+            return Container(
+              child: Text(index.toString()),
+            );
+          }, childCount: 4),
+        )
+      ],
+    ));
   }
 }
